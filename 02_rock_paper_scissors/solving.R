5f5eb1745cb5b13b7bdfaf854cdf444183f9c5d0
Homer White <homerhanumat@gmail.com>
2022-12-04 20:10:41 -0500

    edit

--- 02_rock_paper_scissors/solving.R
+++ 02_rock_paper_scissors/solving.R
@@ -8,36 +8,17 @@ input <- read_table(
   )
 names(input) <- c("opponent", "you")
 
-rules <- matrix(
-  c(
-    3, 6, 0,
-    0, 3, 6,
-    6, 0, 3
-  ),
-  nrow = 3,
-  byrow = TRUE
+outcomes <-
+  c(3, 6, 0, 0, 3, 6, 6, 0, 3)
+names(outcomes) <- c(
+  "AX", "AY", "AZ", "BX", "BY", "BZ", "CX", "CY", "CZ"
 )
-objects <- c("rock", "paper", "scissors")
-rownames(rules) <- objects
-colnames(rules) <- objects
 
 input_1 <-
   input %>% 
-  mutate(opponent = recode(
-    opponent, A = "rock", B = "paper", C = "scissors")
-  )%>% 
-  mutate( you = recode(
-    you, X = "rock", Y = "paper", Z = "scissors")
-  ) %>% 
-  mutate(outcome_score = pmap_dbl(
-    list(opponent, you),
-    function(x, y) rules[x, y]
-  )) %>% 
-  mutate(shape_score = recode(
-    you,
-    "rock" = 1, "paper" = 2, "scissors" = 3
-    )
-  ) %>% 
+  mutate(together = str_c(opponent, you, sep = "")) %>% 
+  mutate(outcome_score = outcomes[together]) %>% 
+  mutate(shape_score = recode(you, X = 1, Y = 2, Z = 3)) %>% 
   mutate(score = outcome_score + shape_score)
 
 answer_1 <-
@@ -63,17 +44,7 @@ input_2 <-
   input %>% 
   mutate(outcome_score = recode(you, X =  0, Y = 3, Z = 6)) %>% 
   mutate(together = str_c(opponent, you, sep = "")) %>% 
-  mutate(your_object = recode(together,
-                              AX = "scissors",
-                              AY = "rock",
-                              AZ = "paper",
-                              BX = "rock",
-                              BY = "paper",
-                              BZ = "scissors",
-                              CX = "paper",
-                              CY = "scissors",
-                              CZ = "rock"
-                              )) %>% 
+  mutate(your_object = your_play_decrypted[together]) %>% 
   mutate(shape_score = recode(
     your_object, "rock" = 1, "paper" = 2, "scissors" = 3)) %>% 
   mutate(score = shape_score + outcome_score)
